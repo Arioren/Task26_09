f1b80af0d543f3160d5f091d0bfb1255599a2255
ari oren <arioren100@gmail.com>
2024-09-26 15:33:59 +0300

    sql

---
+++ question 4.sql
@@ -0,0 +1,17 @@
+--1
+SELECT air_force, COUNT(*), target_city
+FROM public.mission
+WHERE EXTRACT(YEAR FROM mission_date) = 1942
+GROUP BY target_city , air_force
+ORDER BY COUNT(*) DESC
+LIMIT 1
+
+--2
+SELECT Bomb_Damage_Assessment , COUNT(*), target_city
+FROM public.mission
+WHERE
+COALESCE(airborne_aircraft, 0) + COALESCE(attacking_aircraft, 0) + COALESCE(bombing_aircraft, 0) > 5
+AND Bomb_Damage_Assessment IS NOT NULL
+GROUP BY Bomb_Damage_Assessment, target_city
+ORDER BY COUNT(*) DESC
+LIMIT 1
