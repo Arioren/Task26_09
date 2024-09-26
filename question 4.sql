96a007e95d3861a3b3a81ac10d8cd36a8ceb5e3f
ari oren <arioren100@gmail.com>
2024-09-26 16:06:50 +0300

    fix sql

--- question 4.sql
+++ question 4.sql
@@ -7,11 +7,11 @@ ORDER BY COUNT(*) DESC
 LIMIT 1
 
 --2
-SELECT Bomb_Damage_Assessment , COUNT(*), target_city
+SELECT Bomb_Damage_Assessment , COUNT(target_country)
 FROM public.mission
 WHERE
 COALESCE(airborne_aircraft, 0) + COALESCE(attacking_aircraft, 0) + COALESCE(bombing_aircraft, 0) > 5
 AND Bomb_Damage_Assessment IS NOT NULL
-GROUP BY Bomb_Damage_Assessment, target_city
-ORDER BY COUNT(*) DESC
+GROUP BY Bomb_Damage_Assessment, target_country
+ORDER BY COUNT(Bomb_Damage_Assessment) DESC
 LIMIT 1
